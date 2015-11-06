7f67b081d5d5a30efdef5d139e32e59644582a83
Skypeace <hertnet_nonza@hotmail.com>
2015-11-06 16:40:33 +0700

    Snake

---
+++ Assets/Script/FoodSpawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodSpawn : MonoBehaviour {
+
+	public GameObject RatPrefab;
+
+	public Transform RightRim;
+	public Transform LeftRim;
+	public Transform BottomRim;
+	public Transform TopRim;
+
+	void Start () {
+		InvokeRepeating("Spawn", 2, 4);
+	}
+	void Spawn(){
+
+		int x = (int)Random.Range( LeftRim.position.x, RightRim.position.x);
+		int y = (int)Random.Range( BottomRim.position.y, TopRim.position.y);
+		Instantiate(RatPrefab, new Vector2(x, y), Quaternion.identity);
+	}
+}
---
+++ Assets/Script/Snake.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Snake : MonoBehaviour {
+
+	Vector2 di = Vector2.right;
+	List<Transform> tail = new List<Transform>();
+	bool ate = false;
+	public GameObject TailPrefab;
+
+
+	void Start () {
+		InvokeRepeating("Move", 0.1f, 0.1f);
+	}
+
+	void Update () {
+		if (Input.GetKey(KeyCode.RightArrow))
+			di = Vector2.right;
+		else if (Input.GetKey(KeyCode.DownArrow))
+			di = -Vector2.up;
+		else if (Input.GetKey(KeyCode.LeftArrow))
+			di = -Vector2.right;
+		else if (Input.GetKey(KeyCode.UpArrow))
+			di = Vector2.up;
+	}
+	void Move() {
+		transform.Translate(di);
+		Vector2 v = transform.position;
+		if (ate) {
+			GameObject g =(GameObject)Instantiate(TailPrefab, v, Quaternion.identity);
+
+			tail.Insert(0, g.transform);
+
+			ate = false;
+		}
+
+		else if (tail.Count > 0) {
+			tail.Last().position = v;
+
+			tail.Insert(0, tail.Last());
+			tail.RemoveAt(tail.Count-1); 
+		}
+	}
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.name.StartsWith("RatPrefab")) {
+				ate = true;
+				Destroy(coll.gameObject);
+		}
+		else if (coll.name.StartsWith("TailPrefab")) {
+			Invoke("Load",0);
+		}
+		else {
+			Invoke("Load",0);
+		}
+		}
+	public void Load (){
+		Application.LoadLevel("Snake");
+	}
+
+}
